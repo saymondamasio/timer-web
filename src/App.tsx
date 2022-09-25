a0e40e9b37769112b6455ba51d831a7c1aec3f2d
Saymon Damásio <saymon.damasio95@gmail.com>
2022-09-24 21:24:16 -0300

    define theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App(): JSX.Element {
-	return <div className="App"></div>
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Button />
+			<GlobalStyle />
+		</ThemeProvider>
+	)
 }
