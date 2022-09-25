3ce2dead9dabadf800a7e3e01ef01c7f3945bfcf
Saymon Damásio <saymon.damasio95@gmail.com>
2022-09-25 17:53:46 -0300

    config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App(): JSX.Element {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<Button />
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
 			<GlobalStyle />
 		</ThemeProvider>
 	)
